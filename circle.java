2ebb8b36a8e14a2fbe01fb34347a4a83ca0c1af3
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-08 14:26:54 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,10 @@
+public class circle
+{
+
+       public static void main(String arg[])
+       {
+           int r=10;
+           double area=3.14*r*r;
+           System.out.println("area of circle="+area);
+       }
+}
