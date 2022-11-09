ee2e6730cc9cccf03a49b5b31581cb311183814a
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-09 22:30:58 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class Evenodd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n;
+        System.out.println("Enten a no:");
+        n=sc.nextInt();
+        if(n%2==0)
+        System.out.println("Even no");
+        else
+        System.out.println("Odd no");
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
