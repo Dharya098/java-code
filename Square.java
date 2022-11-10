ab5fd4a384feb1246d7090cb092cdbd3a9a03371
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-10 22:12:28 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,12 @@
+
+public class Square
+{
+    public static void main(String arg[])
+    {
+        int n=4;
+        double s;
+        s=Math.pow(n, 2);
+        System.out.println("4 power 2 is:"+s);
+        
+    }
+}
---
+++ simpleintrest.java
@@ -0,0 +1,13 @@
+public class simpleintrest
+{
+public static void main(String arg[])
+    {
+    int p=50;
+    int t=40;
+    int r=10;
+    double si=(p*t*r)/100.0;
+    System.out.println("simple intrest="+si);
+    double ta=p+si;
+    System.out.println("total amount="+ta);
+    }
+}
