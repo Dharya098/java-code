2c76ad795a7a698afe8ffe1d698cdf42a2a67e10
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-12 17:39:27 +0530

    Add files via upload

---
+++ Cylinder.java
@@ -0,0 +1,11 @@
+public class Cylinder
+{
+    public static void main(String arg[])
+    {
+        int r=40;
+        int h=30;
+        double Surfacearea=2*3.14*r*(h+r);
+        System.out.println("surface area=" +Surfacearea);
+        
+    }
+}
