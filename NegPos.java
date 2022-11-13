3a81fe9578e4198eb59efa03769c6f668272d586
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-13 23:24:12 +0530

    Add files via upload

---
+++ NegPos.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class NegPos
+{
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number");
+        int a;
+        a = sc.nextInt();
+        if(a<0)
+        {
+            System.out.println("Thae Number Is Negative");
+        }
+        else
+        {
+            System.out.print("The Number Is Posetive");
+        }
+        
+    }
+    
+}
