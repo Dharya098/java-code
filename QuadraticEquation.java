2c425a9119abf7fa18c88fa36b8d40699c4d7bfc
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-14 22:59:45 +0530

    Add files via upload

---
+++ QuadraticEquation.java
@@ -0,0 +1,11 @@
+public class QuadraticEquation
+{
+    public static void main(String arg[])
+    {
+        int a=10;
+        int b=20;
+        int c=-10;
+        double quadraticequation=((-b+Math.sqrt(b*b)-(4*a*c))/(2*a));
+        System.out.println("Quadraticequation=" +quadraticequation);
+    }
+}
