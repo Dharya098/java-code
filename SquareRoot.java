ec653ac7caebf0f6a3576bd76ba21bccdce05e6f
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-15 23:10:12 +0530

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,12 @@
+public class SquareRoot
+{
+    public static void main(String arg[])
+    {
+        int n=800;
+        double s;
+        s=Math.sqrt(n);
+        System.out.println("squareroot of"+ n +"is:"+s);
+        
+        
+    }
+}
