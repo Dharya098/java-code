820b539c04a0732357c049b13c6b5a2a4aeb4dbb
Dharya098 <117718639+Dharya098@users.noreply.github.com>
2022-11-16 20:17:19 +0530

    Add files via upload

---
+++ Tempreture.java
@@ -0,0 +1,13 @@
+public class Tempreture
+{
+    public static void main(String arg[])
+    {
+        double c,f;
+        c=98;
+        f=((9*c)/5)+32;
+        System.out.println("Tem.in.F.:"+f);
+        
+    }
+    
+     
+}
