b47eb00406774cad56e5cbd343d7a99be76e5397
Dharya <117718639+Dharya098@users.noreply.github.com>
2023-01-18 15:47:46 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class forloop
+{
+    public static void main(String arg[])
+    {
+        int i,k=0;
+        for(i=1;i<=100;i++)
+        k=k+i;
+        System.out.println("sum of all number between 1to100="+k);
+    }
+}
