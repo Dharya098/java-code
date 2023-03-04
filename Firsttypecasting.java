cb9de56d4275b5bc3ca693aca432f81b03a99f48
Dharya <117718639+Dharya098@users.noreply.github.com>
2023-03-04 22:50:33 +0530

    Add files via upload

---
+++ Firsttypecasting.java
@@ -0,0 +1,9 @@
+public class Firsttypecasting {
+    public static void main(String[] args) {
+        int num = 9;
+        double num2 = num;
+
+        System.out.println(num);
+        System.out.println(num2);
+    }
+}
